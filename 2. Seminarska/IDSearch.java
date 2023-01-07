63c42fe56614c2cba07f38786ab465d70a3cd3ea
irtgal <gal.irt.01@gmail.com>
2023-01-07 17:38:49 +0100

    ID search

--- 2. Seminarska/IDSearch.java
+++ 2. Seminarska/IDSearch.java
@@ -2,42 +2,45 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
+import java.util.*;
 
 public class IDSearch {
 
-    public static void search(Warehouse warehouse) {
+    public static List<Warehouse.Move> search(Warehouse warehouse) {
         System.out.println(warehouse);
         for (int depthLimit = 0; depthLimit < Integer.MAX_VALUE; depthLimit++) {
             System.out.println("Globina iskanja je " + depthLimit);
 
             Stack<Warehouse> stack = new Stack<>();
-            ArrayList<Warehouse> visited = new ArrayList<>();
+            Set<String> visited = new HashSet<>();
 
             stack.push(warehouse);
-            visited.add(warehouse);
+            visited.add(warehouse.toString());
 
-            StringBuilder mvs = new StringBuilder();
+            List<Warehouse.Move> foundMoves = new ArrayList<>();
             while (!stack.isEmpty()) {
-                Warehouse curState = stack.peek();
-                if (curState.equals(warehouse)) mvs.setLength(0);
-                if (curState.isSolved()) {
-                    System.out.println("Resitev IDDFS v stanju " + curState);
-                    System.out.println(mvs);
-                    return;
+                Warehouse currentState = stack.peek();
+                // To mi ni cist jasno, zakaj je tole potrebno
+                // if (curState.equals(warehouse))
+                // mvs.setLength(0);
+                if (currentState.isSolved()) {
+                    System.out.println("Resitev IDDFS : " + currentState.getMoves().size() + " korakov");
+                    foundMoves = currentState.getMoves();
+                    System.out.println(foundMoves);
+                    return foundMoves;
                 }
 
                 boolean found = false;
-                if (curState.getMoves().size() <= depthLimit) {
+                if (currentState.getMoves().size() <= depthLimit) {
                     // najdi neobiskano naslednje stanje
-                    for (int fromCol = 0; fromCol < curState.numCols; fromCol++) {
-                        for (int toCol = 0; toCol < curState.numCols; toCol++) {
-                            if (curState.canMove(fromCol, toCol)) {
-                                Warehouse nextState = curState.deepClone();
+                    for (int fromCol = 0; fromCol < currentState.numCols; fromCol++) {
+                        for (int toCol = 0; toCol < currentState.numCols; toCol++) {
+                            if (currentState.canMove(fromCol, toCol)) {
+                                Warehouse nextState = currentState.deepClone();
                                 String mv = nextState.move(fromCol, toCol).toString();
-                                if (!containsState(visited, nextState.state)) {
-                                    mvs.append(mv).append("\n");
+                                if (!visited.contains(nextState.toString())) {
                                     stack.push(nextState);
-                                    visited.add(nextState);
+                                    visited.add(nextState.toString());
                                     found = true;
                                     break;
                                 }
@@ -56,7 +59,9 @@ public class IDSearch {
 
             System.out.println("-----------------------------------------------------------");
         }
+        return new ArrayList<Warehouse.Move>();
     }
+
     public static boolean containsState(ArrayList<Warehouse> list, char[][] state) {
         for (Warehouse warehouse : list) {
             if (Arrays.deepEquals(warehouse.state, state)) {
@@ -65,12 +70,13 @@ public class IDSearch {
         }
         return false;
     }
+
     public static void main(String[] args) throws IOException {
-        String initialFile = "2. Seminarska/primer2_zacetna.txt";
-        String finalFile = "2. Seminarska/primer2_koncna.txt";
+        String initialFile = "primer4_zacetna.txt";
+        String finalFile = "primer4_koncna.txt";
         char[][] initialState = Warehouse.readStateFromFile(initialFile);
         char[][] finalState = Warehouse.readStateFromFile(finalFile);
-        Warehouse w = new Warehouse(initialState,finalState);
+        Warehouse w = new Warehouse(initialState, finalState);
         search(w);
     }
 }
